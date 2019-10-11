d77ca488aca59d3fe1123ea7b99d0213981bc09d
Brian Han <brhan@indeed.com>
2019-10-11 17:09:48 -0500

    chore: npm run dev runs demo and rebuilds rollup

---
+++ demo/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import { render } from "react-dom"
+import Example from "../build/index.esm"
+
+render(<Example></Example>, document.getElementById("App"))
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react"
 import styles from "./index.scss"
+import "./index.css"
+
 import IconSvg from "./icon.svg"
 import pkg from "../package.json"
 
 const Example = ({ withIconImg }) => (
-  <button className={styles.example} type="button">
+  <button onClick={() => alert("hi")} className={styles.example} type="button">
     {/* <IconSvg /> */}
     <img src={IconSvg} alt="icon" />
-    <span>Hello, this is an example component from: {pkg.name}</span>
+    <p className={styles.text}>
+      Hello, this is an example component from: {pkg.name}
+    </p>
   </button>
 )
 
